8ff1ff96202428fd42ac72531a1d04363425e87e
Vinícius Silveira <129103673+ViniSV@users.noreply.github.com>
2024-06-10 18:21:30 -0300

    Novos arquivos php com conexão definitiva

---
+++ php/Signin.php
@@ -0,0 +1,31 @@
+<?php
+$server_name = "db4free.net:3306";
+$user_name = "aula_salvador224";
+$password = "teste2024";
+$database_name = "banco_salvador25";
+
+$conn = new mysqli($server_name, $user_name, $password, $database_name);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error . "<br>");
+}
+
+//Form data client
+$client_name = $_POST['userName'];
+$client_email = $_POST['userEmail'];
+$client_password = $_POST['userPassword'];
+$client_number = $_POST['userNumber'];
+$client_CEP = $_POST['userCEP'];
+
+//Sending client data
+$stmt_cliente = $conn->prepare("INSERT INTO client (name, email, password, number, cep) VALUES (?, ?, ?, ?, ?)");
+$stmt_cliente->bind_param("sssii", $client_name, $client_email, $client_password, $client_number, $client_CEP);
+
+if ($stmt_cliente->execute()) {
+    echo "Cliente inserido no banco de dados com sucesso! <br>";
+} else {
+    echo "Não foi possível inserir os dados do cliente no nosso banco de dados, por favor confira os dados informados!" . $stmt_cliente->error . "<br>";
+}
+$stmt_cliente->close();
+$conn->close();
+?>
---
+++ php/deleteAppt.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    die("Acesso negado: usuário não está logado");
+}
+
+if (isset($_GET['id'])) {
+    $appointment_id = $_GET['id'];
+
+    // Dados de conexão com o banco de dados
+    $server_name = "db4free.net:3306";
+    $user_name = "aula_salvador224";
+    $password = "teste2024";
+    $database_name = "banco_salvador25";
+
+    $conn = new mysqli($server_name, $user_name, $password, $database_name);
+
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error . "<br>");
+    }
+
+    // Consulta SQL para excluir a consulta
+    $sql = "DELETE FROM appointment WHERE id = ? AND client_id = ?";
+
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt === false) {
+        die("Erro na preparação: " . $conn->error);
+    }
+
+    $stmt->bind_param("ii", $appointment_id, $_SESSION['user_id']);
+
+    $stmt->execute();
+
+    // Fechamentos
+    $stmt->close();
+    $conn->close();
+
+    header('Location: checkAppt.php');
+    exit();
+} else {
+    die("ID da consulta não fornecido.");
+}
